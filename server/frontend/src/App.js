aeb989161a591af5690f2c7b33f5006119585960
walterbaya <56239454+walterbaya@users.noreply.github.com>
2024-08-30 18:41:27 -0300

    me olvide de subir los cambios

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -3,11 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import Dealers from './components/Dealers/Dealers';
 import Dealer from "./components/Dealers/Dealer";
 import PostReview from "./components/Dealers/PostReview"
+import Register from "./components/Register/Register.jsx";
 
 function App() {
     return (
         <Routes>
             <Route path="/login" element={<LoginPanel />} />
+            <Route path="/register" element={<Register />} />
             <Route path="/dealers" element={<Dealers />} />
             <Route path="/dealer/:id" element={<Dealer />} />
             <Route path="/postreview/:id" element={<PostReview />} />
