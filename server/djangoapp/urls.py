d83ee75886c8cf3a4049463b9433fb743e05c26c
walterbaya <56239454+walterbaya@users.noreply.github.com>
2024-08-30 17:30:46 -0300

    refactorizo otra vez

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,3 +1,4 @@
+# Uncomment the imports before you add the code
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
@@ -8,10 +9,17 @@ urlpatterns = [
     # # path for registration
 
     # path for login
-    path(route='login', view=views.login_user, name='login'),
+    # path(route='login', view=views.login_user, name='login'),
 
     # path for dealer reviews view
-
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     # path for add a review view
+    path(route='add_review', view=views.add_review, name='add_review'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
+
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,17 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer";
+import PostReview from "./components/Dealers/PostReview"
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/login" element={<LoginPanel />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/login" element={<LoginPanel />} />
+            <Route path="/dealers" element={<Dealers />} />
+            <Route path="/dealer/:id" element={<Dealer />} />
+            <Route path="/postreview/:id" element={<PostReview />} />
+        </Routes>
+    );
 }
 export default App;
