4b7f5477bb335bd05b7496bd921aa4be0af5981e
walterbaya <56239454+walterbaya@users.noreply.github.com>
2024-09-03 00:18:26 -0300

    Se modifica settings y restapis

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,11 +30,13 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "localhost",
     "https://walterbaya19-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
-    "https://walterbaya19-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai","https://sentianalyzer.1ld4fm6f9sfr.us-south.codeengine.appdomain.cloud"
+    "https://walterbaya19-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai",
+    "https://sentianalyzer.1ld4fm6f9sfr.us-south.codeengine.appdomain.cloud",
 ]
 CSRF_TRUSTED_ORIGINS = [
     "https://walterbaya19-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
-    "https://walterbaya19-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai", "https://sentianalyzer.1ld4fm6f9sfr.us-south.codeengine.appdomain.cloud"
+    "https://walterbaya19-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai",
+    "https://sentianalyzer.1ld4fm6f9sfr.us-south.codeengine.appdomain.cloud",
 ]
 
 REST_FRAMEWORK = {
