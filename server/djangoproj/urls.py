2fc490fbf8600d3f7a7a9adf0d7520b279d631be
walterbaya <56239454+walterbaya@users.noreply.github.com>
2024-09-03 14:29:01 -0300

    Se corrije indentaicon

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -13,9 +13,9 @@ urlpatterns = [
     path("about/", TemplateView.as_view(template_name="About.html")),
     path("dealers/", TemplateView.as_view(template_name="index.html")),
     path("dealer/<int:dealer_id>",
-        TemplateView.as_view(template_name="index.html")),
+         TemplateView.as_view(template_name="index.html")),  # Indentación corregida
     path("djangoapp/", include("djangoapp.urls")),
     path("postreview/<int:dealer_id>",
-        TemplateView.as_view(template_name="index.html")),
+         TemplateView.as_view(template_name="index.html")),  # Indentación corregida
     path("", TemplateView.as_view(template_name="Home.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
