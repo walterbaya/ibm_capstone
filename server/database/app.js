72e19bb40351bf3221971932b6401720022ad851
walterbaya <56239454+walterbaya@users.noreply.github.com>
2024-09-03 15:08:02 -0300

    Se repara todo linteado

--- server/database/app.js
+++ server/database/app.js
@@ -35,7 +35,7 @@ try {
 
 // Express route to home
 app.get('/', async (req, res) => {
-    res.send("Welcome to the Mongoose API")
+    res.send("Welcome to the Mongoose API");
 });
 
 // Express route to fetch all reviews
@@ -94,8 +94,8 @@ app.get('/fetchDealer/:id', async (req, res) => {
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
     data = JSON.parse(req.body);
-    var documents = await Reviews.find().sort({ id: -1 })
-    var new_id = documents[0].id + 1
+    var documents = await Reviews.find().sort({ id: -1 });
+    var new_id = documents[0].id + 1;
 
     var review = new Reviews({
         "id": new_id,
