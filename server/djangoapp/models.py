af7ec0ff5615c3851c64af25fb1fc78b024676c6
walterbaya <56239454+walterbaya@users.noreply.github.com>
2024-09-03 00:46:32 -0300

    Se arreglan linteos models, populate y restapis

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -47,7 +46,6 @@ class CarModel(models.Model):
     year = models.IntegerField(
         default=2023, validators=[MaxValueValidator(2023), MinValueValidator(2015)]
     )
-    # Other fields as needed
 
     def __str__(self):
         return self.name  # Return the name as the string representation
--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -26,7 +26,7 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception:
         # If any error occurs
         print("Network exception occurred")
 
@@ -52,5 +52,5 @@ def post_review(data_dict):
         response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
+    except Exception:
         print("Network exception occurred")
